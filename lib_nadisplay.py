d4ca90619008604feac6596d573e9008524926aa
nath54 <cerisara.nathan@gmail.com>
2024-12-10 20:33:07 +0100

    Travail Create ND_LineEdit, ND_LineEdit has too be implemented yet

--- lib_nadisplay.py
+++ lib_nadisplay.py
@@ -2162,6 +2162,111 @@ class ND_V_ScrollBar(ND_Elt):
                 self.scroll_position = max(0, min(self.content_height - self.h,
                                                   int(relative_y * self.content_height / self.h)))
 
+#
+class ND_LineEdit(ND_Elt):
+    #
+    def __init__(
+            self,
+            window: ND_Window,
+            elt_id: str,
+            position: ND_Position,
+            text: str = "",
+            place_holder: str = "",
+            mouse_active: bool = True,
+            font_name: Optional[str] = None,
+            font_size: int = 24,
+            border_radius: int = 5,
+            border: bool = True,
+            base_bg_color: Optional[ND_Color] = None,
+            base_fg_color: Optional[ND_Color] = None,
+            hover_bg_color: Optional[ND_Color] = None,
+            hover_fg_color: Optional[ND_Color] = None,
+            clicked_bg_color: Optional[ND_Color] = None,
+            clicked_fg_color: Optional[ND_Color] = None,
+
+    ) -> None:
+        #
+        super().__init__(window=window, elt_id=elt_id, position=position)
+        #
+        self.state: str = "normal"
+        #
+        self.text: str = text
+        self.font_name: Optional[str] = font_name
+        self.font_size: int = font_size
+        self.base_bg_color: ND_Color = base_bg_color if base_bg_color is not None else cl("gray")
+        self.base_fg_color: ND_Color = base_fg_color if base_fg_color is not None else cl("black")
+        self.hover_bg_color: ND_Color = hover_bg_color if hover_bg_color is not None else cl("dark gray")
+        self.hover_fg_color: ND_Color = hover_fg_color if hover_fg_color is not None else cl("black")
+        self.clicked_bg_color: ND_Color = clicked_bg_color if clicked_bg_color is not None else cl("very dark gray")
+        self.clicked_fg_color: ND_Color = clicked_fg_color if clicked_fg_color is not None else cl("black")
+        #
+        self.border: bool = border
+        self.border_radius: int = border_radius
+        #
+        self.base_text_w: int = 0
+        self.base_text_h: int = 0
+        #
+        self.base_text_w, self.base_text_h = get_font_size(self.text, self.font_size, font_ratio=0.5)
+        #
+        self.cursor: int = 0
+        self.cursor_width: int = 2
+        self.cursor_height: int = self.font_size
+        self.cursor_visible: int = 0
+        self.cursor_last_state_change: float = time.time()
+        self.cursor_change_speed: float = 0.1
+        self.cursor_color: Optional[ND_Color] = None  # Font color if none
+        #
+        # TODO: H_Scrollbar
+
+    #
+    def on_key_press(self) -> None:
+        # TODO
+        pass
+
+
+    #
+    def render(self) -> None:
+        #
+        if not self.visible:
+            return
+
+        #
+        x, y = self.x, self.y
+
+        #
+        bg_color: ND_Color
+        fg_color: ND_Color
+
+        # Getting the right colors along the state
+        if self.state == "normal":
+            bg_color, fg_color = self.base_bg_color, self.base_fg_color
+        elif self.state == "hover":
+            bg_color, fg_color = self.hover_bg_color, self.hover_fg_color
+        else:  # clicked
+            bg_color, fg_color = self.clicked_bg_color, self.clicked_fg_color
+
+        # Drawing the background rect color
+        if not self.border and self.border_radius <= 0:
+            self.window.draw_filled_rect(x, y, self.w, self.h, bg_color)
+        elif not self.border:
+            self.window.draw_rounded_rect(x, y, self.w, self.h, self.border_radius, bg_color, cl((0, 0, 0, 0)))
+        else:
+            self.window.draw_rounded_rect(x, y, self.w, self.h, self.border_radius, bg_color, fg_color)
+
+        # TODO: move the camera along the scrollbar
+
+        #
+        self.window.draw_text(
+                txt=self.text,
+                x=x + (self.w - self.base_text_w) // 2,
+                y=y + (self.h - self.base_text_h) // 2,
+                font=self.font_name,
+                font_size=self.font_size,
+                font_color=fg_color
+        )
+
+        # TODO: draw cursor and cursor visibility change
+
 
 # ND_Container class implementation
 class ND_Container(ND_Elt):
